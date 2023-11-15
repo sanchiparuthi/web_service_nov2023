f8b2a5e18f5696f321915a90924144b3e8505e57
Your Name <paruthisanchi02@gmail.com>
2023-11-15 17:13:30 -0500

    commit2

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("hi")
+import express from 'express'
+
+const app = express()
+
+const port = proces.env.PORT || 3001
+
+app.get('/', (req, res) => {
+    res.send ()
+
+})
+
+app.listen(port, () => {
+    
+})
